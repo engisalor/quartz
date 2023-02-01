54e2e7257e6247c0e1890c84401fff2e58e8e943
engisalor <50170623+engisalor@users.noreply.github.com>
2023-02-01 17:54:29 +0100

    fix: add caching to debug page

--- builtin/pages/debug.py
+++ builtin/pages/debug.py
@@ -1,13 +1,19 @@
+import datetime
 import pathlib
 
 import dash
 import sgex
-from dash import html
+from dash import dcc, get_app, html
 from dash.dependencies import Input, Output
+from flask_caching import Cache
 
 from builtin.utils import decorator
 from builtin.version import version
-from environment.settings import NOSKE_SERVER_NAME
+from environment.settings import NOSKE_SERVER_NAME, cache_config
+
+app = get_app()
+cache = Cache(app.server, config=cache_config)
+
 
 dash.register_page(__name__)
 
@@ -25,6 +31,16 @@ layout = html.Div(
         html.H2("API connections"),
         html.Button("NoSkE", id="check-noske"),
         html.Div(id="check-noske-out", children=""),
+        html.H2("Caching"),
+        html.Div(id="flask-cache-memoized-children"),
+        dcc.RadioItems(
+            [f"Option {i}" for i in range(1, 4)],
+            "Option 1",
+            id="flask-cache-memoized-dropdown",
+        ),
+        html.Div(
+            f'Results are cached for {cache_config["CACHE_DEFAULT_TIMEOUT"]} seconds'
+        ),
     ]
 )
 
@@ -47,7 +63,9 @@ def check_noske(n_clicks):
 
 
 @dash.callback(
-    Output("check-storage-out", "children"), Input("check-storage", "n_clicks")
+    Output("check-storage-out", "children"),
+    Input("check-storage", "n_clicks"),
+    prevent_initial_call=True,
 )
 def check_storage(n_clicks):
     cwd = pathlib.Path.cwd()
@@ -77,3 +95,14 @@ def check_cpu(n_clicks):
 
     _, t = slow_func(10000000)
     return html.Div([html.P(f"Clicks: {n_clicks}"), html.P(f"Execution time: {t}")])
+
+
+@dash.callback(
+    Output("flask-cache-memoized-children", "children"),
+    Input("flask-cache-memoized-dropdown", "value"),
+    prevent_initial_call=True,
+)
+@cache.memoize()
+def render(value):
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    return f'Selected "{value}" at "{current_time}"'
