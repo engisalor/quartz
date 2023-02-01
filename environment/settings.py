03dd9f72755b733cbaf16ea218b8626a4abcf6cb
engisalor <50170623+engisalor@users.noreply.github.com>
2023-02-01 17:52:24 +0100

    fix: add cache settings to environment

--- environment/settings.py
+++ environment/settings.py
@@ -7,9 +7,29 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), os.getenv("ENVIRONMENT_FILE"))
 load_dotenv(dotenv_path=dotenv_path, override=True)
 
+# dash settings
 HOST = os.environ.get("HOST")
 PORT = int(os.environ.get("PORT"))
 DASH_DEBUG = bool(os.environ.get("DASH_DEBUG"))
+
+# cache settings
+if os.environ.get("CACHE_REDIS_HOST"):
+    cache_config = {
+        "CACHE_TYPE": "RedisCache",
+        "CACHE_DEFAULT_TIMEOUT": int(os.environ.get("CACHE_DEFAULT_TIMEOUT")),
+        "CACHE_REDIS_HOST": os.environ.get("CACHE_REDIS_HOST"),
+        "CACHE_REDIS_PASSWORD": os.environ.get("CACHE_REDIS_PASSWORD"),
+        "CACHE_REDIS_PORT": int(os.environ.get("CACHE_REDIS_PORT")),
+    }
+else:
+    cache_config = {
+        "CACHE_TYPE": "FileSystemCache",
+        "CACHE_DEFAULT_TIMEOUT": int(os.environ.get("CACHE_DEFAULT_TIMEOUT")),
+        "CACHE_DIR": os.environ.get("CACHE_DIR"),
+        "CACHE_THRESHOLD": int(os.environ.get("CACHE_THRESHOLD")),
+    }
+
+# default locations
 NOSKE_SERVER_NAME = os.environ.get("NOSKE_SERVER_NAME")
 ASSETS_DIR = os.environ.get("ASSETS_DIR")
 PAGES_DIR = os.environ.get("PAGES_DIR")
