c34458c946031f04a6487657ae60344a71d7b997
engisalor <50170623+engisalor@users.noreply.github.com>
2023-02-05 22:48:46 +0100

    fix: add background callback mgr

--- app.py
+++ app.py
@@ -3,10 +3,21 @@ import importlib
 import dash
 import dash_bootstrap_components as dbc
 import flask
-from dash import html
+from dash import CeleryManager, DiskcacheManager, html
 
 import environment.settings as env
 
+if env.cache_config.get("CACHE_REDIS_HOST"):
+    host = env.cache_config.get("CACHE_REDIS_HOST")
+    from celery import Celery
+
+    celery_app = Celery(__name__, broker=host, backend=host)
+    background_callback_manager = CeleryManager(celery_app)
+else:
+    import diskcache
+
+    cache = diskcache.Cache(".cache")
+    background_callback_manager = DiskcacheManager(cache)
 
 
 layout = importlib.import_module(env.LAYOUT_MODULE)
@@ -15,6 +26,7 @@ server = flask.Flask(__name__)
 app = dash.Dash(
     __name__,
     use_pages=True,
+    background_callback_manager=background_callback_manager,
     pages_folder=env.PAGES_DIR,
     server=server,
     suppress_callback_exceptions=True,
@@ -32,4 +44,6 @@ app.layout = html.Div([layout.sidebar(), layout.content])
 
 
 if __name__ == "__main__":
-    app.run(host=env.HOST, port=env.PORT, debug=env.DASH_DEBUG, dev_tools_hot_reload=False)
+    app.run(
+        host=env.HOST, port=env.PORT, debug=env.DASH_DEBUG, dev_tools_hot_reload=False
+    )
