aa90b92f14f7373a90d2fae1fe7487d8785da77f
engisalor <50170623+engisalor@users.noreply.github.com>
2023-02-05 22:56:24 +0100

    fix: add corpora, graph label settings

--- environment/settings.py
+++ environment/settings.py
@@ -1,4 +1,4 @@
-# settings.py
+import collections
 import os
 from os.path import dirname, join
 
@@ -38,8 +38,22 @@ PAGES_DIR = os.environ.get("PAGES_DIR")
 LAYOUT_MODULE = os.environ.get("LAYOUT_MODULE")
 CORPORA_FILE = os.environ.get("CORPORA_FILE")
 
-# corpora
+# corpora settings
 if CORPORA_FILE:
     corpora = io.load_yaml(CORPORA_FILE)
+    skips = ["name"]
+    attrs = [a for v in corpora.values() for a in v.keys() if a not in skips]
+    comparable_attributes = [i for i, c in collections.Counter(attrs).items() if c > 1]
 else:
     corpora = None
+    comparable_attributes = None
+
+# graph labels
+labels = {
+    "frq": "occurrences",
+    "rel": "relative density %",
+    "reltt": "relative fpm",
+    "fpm": "frequency per million",
+    "nicearg": "query",
+    "corpname": "corpus",
+}
