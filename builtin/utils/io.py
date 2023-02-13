454ea3e90fd87575549f24e690c099911b03070b
engisalor <50170623+engisalor@users.noreply.github.com>
2023-02-13 23:40:41 +0100

    fix(utils): add load, compile markdown funcs

--- builtin/utils/io.py
+++ builtin/utils/io.py
@@ -4,10 +4,22 @@ import yaml
 
 
 def load_yaml(file: str):
-    filepath = pathlib.Path(file)
-    if filepath.exists():
-        with open(filepath, "r") as stream:
-            dt = yaml.safe_load(stream)
-    else:
-        raise FileNotFoundError()
+    with open(file) as stream:
+        dt = yaml.safe_load(stream)
     return dt
+
+
+def load_markdown(file: str, clean_header=True):
+    with open(file) as f:
+        lines = f.readlines()
+    if clean_header:
+        lines = [x.lstrip("#") for x in lines]
+    return "".join(lines)
+
+
+def compile_markdown(dir: str, clean_header=False):
+    files = pathlib.Path(dir).glob("*.md")
+    files = sorted(files)
+
+    texts = [load_markdown(f, clean_header) for f in files]
+    return "\n\n".join(texts)
