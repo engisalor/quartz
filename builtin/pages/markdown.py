b662759300e3bc6fe6f97a2861c2570f768e2d5d
engisalor <50170623+engisalor@users.noreply.github.com>
2023-03-15 23:11:31 +0100

    fix: remove debug, md cheatsheet pages

--- builtin/pages/markdown.py
+++
@@ -1,8 +0,0 @@
-import dash
-from dash import html
-
-from builtin.components.aio.aio import MarkdownFileAIO
-
-dash.register_page(__name__)
-
-layout = html.Div(children=[MarkdownFileAIO("cheatsheet", "builtin/markdown")])
