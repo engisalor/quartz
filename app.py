944f05a372cfa5a14b4719cf0efd8ff1849eecf3
engisalor <50170623+engisalor@users.noreply.github.com>
2023-10-15 16:04:35 +0200

    fix: update app.py

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 import importlib
+from pathlib import Path
 
 import dash
 import dash_bootstrap_components as dbc
 import flask
 from dash import html
 
-import environment.settings as env
 from builtin.utils import redirect
+from environment.settings import env
 
-layout = importlib.import_module(env.LAYOUT_MODULE)
+layout = importlib.import_module(env.ACTIVE_DIR.name.strip("/") + ".layout.layout")
 server = flask.Flask(__name__)
 
 app = dash.Dash(
     __name__,
     use_pages=True,
-    pages_folder=env.PAGES_DIR,
+    pages_folder=env.ACTIVE_DIR / Path("pages"),
     server=server,
     suppress_callback_exceptions=True,
     assets_ignore=".*ignore.*",
-    assets_folder=env.ASSETS_DIR,
+    assets_folder=env.ACTIVE_DIR / Path("assets"),
     external_stylesheets=[
         dbc.themes.CERULEAN,
         dbc.icons.BOOTSTRAP,
