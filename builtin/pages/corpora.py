bfe03cc9091b391650c0ec23dd0a0fc7b4d8d489
engisalor <50170623+engisalor@users.noreply.github.com>
2023-10-15 16:01:59 +0200

    fix: update corpora page

--- builtin/pages/corpora.py
+++ builtin/pages/corpora.py
@@ -1,14 +1,14 @@
+from pathlib import Path
+
 import dash
 import dash_bootstrap_components as dbc
 from dash import Input, Output, get_app, html
-from flask_caching import Cache
 
-import environment.settings as env
 from builtin.components.aio.aio import MarkdownFileAIO
 from builtin.components.aio.corpora import CorpusDetailsAIO
+from environment.settings import corp_data, env
 
 app = get_app()
-cache = Cache(app.server, config=env.FLASK_CACHE_CONFIG)
 
 
 dash.register_page(__name__)
@@ -16,15 +16,15 @@ dash.register_page(__name__)
 
 layout = html.Div(
     [
-        MarkdownFileAIO("builtin/markdown/corpora.md"),
+        MarkdownFileAIO(env.ACTIVE_DIR / Path("markdown/corpora.md")),
         html.Br(),
         dbc.Tabs(
             [
                 dbc.Tab(
-                    label=env.corpora[corpus]["name"],
+                    label=corp_data.dt[corpus]["name"],
                     tab_id=corpus,
                 )
-                for corpus in [k for k in env.corpora.keys()]
+                for corpus in [k for k in corp_data.dt.keys()]
             ],
             id="tabs",
         ),
@@ -34,6 +34,5 @@ layout = html.Div(
 
 
 @dash.callback(Output("content", "children"), Input("tabs", "active_tab"))
-@cache.memoize()
 def switch_tab(corpus):
     return CorpusDetailsAIO(corpus)
