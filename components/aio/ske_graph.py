eaa1340ac1ec3906afac3ac6a860a57bf8330542
engisalor <50170623+engisalor@users.noreply.github.com>
2023-10-25 19:41:19 +0200

    fix: improve aio component for graph group

--- components/aio/ske_graph.py
+++ components/aio/ske_graph.py
@@ -1,104 +1,99 @@
 # Copyright (c) 2023 Loryn Isaacs
 # This file is part of Quartz, licensed under GPL3+ https://github.com/engisalor/quartz
 import json
-import re
 import uuid
-from urllib import parse
 
 import dash_bootstrap_components as dbc
-from dash import MATCH, Input, Output, callback, dcc, html
+import pandas as pd
+from dash import MATCH, Input, Output, State, callback, ctx, dcc, html
+from sgex.job import Job
 
-from settings import env
-
-
-def split_q(q: str | list) -> tuple:
-    """Returns a tuple of (default_attr, cql) from a query (e.g., 'aword,"sun"')."""
-    if isinstance(q, list):
-        return re.search(r"^a(.*?),(.+)", q[0]).groups()
-    else:
-        return re.search(r"^a(.*?),(.+)", q).groups()
+from components.freqs_fig import bar_figure, prep_data
+from settings import corp_data, env
+from utils import url
 
 
-def split_attr_path(attr_path: str) -> tuple:
-    """Returns a tuple of (struct, attr) from an attribute path (e.g., 'doc.file')."""
-    return re.search(r"^([\w]+)\.(.+)", attr_path).groups()
-
-
-def cql(
-    params: dict,
-    base_url: str = "https://app.sketchengine.eu/#concordance?",
-):
-    """Returns a URL from given API call parameters."""
-    _params = params.copy()
-    default_attr, _params["cql"] = split_q(_params["q"])
-    del _params["q"]
-    template = {
-        "tab": "advanced",
-        "queryselector": "cql",
-        "showresults": "1",
-        "default_attr": default_attr,
-    }
-    query = parse.urlencode(_params | template, quote_via=parse.quote)
-    base = parse.urlsplit(base_url, allow_fragments=False)
-    return base._replace(query=query).geturl()
-
-
-def cql_ttype_filter(
-    params: dict,
-    attr_path: str,
-    value: str,
-    base_url: str = "https://app.sketchengine.eu/#concordance?",
-) -> str:
-    """Returns a URL with a text type filter from given API call params."""
-    struct, attr = split_attr_path(attr_path)
-    _filter = f" [] within <{struct} {attr}={json.dumps(value,ensure_ascii=False)} />"
-    _, cql = split_q(params["q"])
-    template = [
-        ("corpname", params["corpname"]),
-        ("tab", "advanced"),
-        ("queryselector", "cql"),
-        ("cql", cql),
-        ("showresults", "1"),
-        (
-            "operations",
-            json.dumps(
-                [
-                    {
-                        "name": "cql",
-                        "arg": cql,
-                        "query": {"queryselector": "cqlrow", "cql": cql},
-                    },
-                    {
-                        "name": "filter",
-                        "arg": _filter,
-                        "query": {
-                            "pnfilter": "p",
-                            "queryselector": "cqlrow",
-                            "inclkwic": True,
-                            "filfpos": "0",
-                            "filtpos": "0<0",
-                            "partial_match": 0,
-                            "cql": _filter,
-                        },
-                    },
-                ],
-                ensure_ascii=False,
-            ),
+def _update_link(point: dict) -> dbc.NavLink:
+    """Generates a nav link based on figure click data."""
+    ls = point.get("customdata")
+    params = json.loads(ls[0])
+    href = url.cql_ttype_filter(
+        params=params,
+        attr_path=ls[1],
+        value=ls[2],
+        base_url=env.SERVER_URL.rstrip("/") + "/#concordance?",
+    )
+    return dbc.NavLink(
+        html.I(
+            className="bi bi-arrow-up-right-square-fill",
+            title="Go to " + corp_data.dt[params["corpname"]]["name"],
+            style={"color": corp_data.dt[params["corpname"]]["color"]},
         ),
-    ]
-    base = parse.urlsplit(base_url, allow_fragments=False)
-    query = parse.urlencode(template, quote_via=parse.quote)
-    return base._replace(query=query).geturl()
+        href=href,
+        target="_blank",
+    )
+
+
+def _params_from_point(point: dict, crossfilter) -> tuple:
+    """Builds API parameters from figure click data point."""
+    params, attr_path, value = point.get("customdata", [None] * 3)[:3]
+    params = json.loads(params)
+    corpus = params["corpname"]
+    label_map = {v: k for k, v in corp_data.dt[corpus]["label"].items()}
+    struct, attr = url.split_attr_path(attr_path)
+    params["call_type"] = "Freqs"
+    params["q"] += f" within <{struct} {attr}={json.dumps(value,ensure_ascii=False)} />"
+    params["fcrit"] = f"{label_map[crossfilter]} 0"
+    # also try: [f"{x} 0" for x in corp_data.dt[_params["corpname"]]["label"].keys()]
+    x_suffix = corp_data.dt[corpus]["label"][attr_path] + f"={value}"
+    return params, x_suffix
+
+
+def _df_from_crossfilter(clickdata: dict, crossfilter, title) -> tuple:
+    """Runs API calls based on figure click data."""
+    params = []
+    for point in clickdata["points"]:
+        _params, x_suffix = _params_from_point(point, crossfilter)
+        params.append(_params)
+    j = Job(params=params, **env.sgex)
+    j.run()
+    dfs = []
+    for call in j.data.freqs:
+        df = call.df_from_json()
+        df["params"] = json.dumps(call.params)
+        df["query"] = title
+        dfs.append(df)
+    return pd.concat(dfs), x_suffix
 
 
 class SkeGraphAIO(html.Div):
     class ids:
-        title_link = lambda aio_id: {  # noqa: E731
-            "type": "SkeGraphAIO",
+        title = lambda aio_id: {  # noqa: E731
+            "type": "Title",
             "group": aio_id,
         }
-        graph = lambda aio_id: {  # noqa: E731
-            "type": "Graph",
+        link1 = lambda aio_id: {  # noqa: E731
+            "type": "Link1",
+            "group": aio_id,
+        }
+        link2 = lambda aio_id: {  # noqa: E731
+            "type": "Link2",
+            "group": aio_id,
+        }
+        graph1 = lambda aio_id: {  # noqa: E731
+            "type": "Graph1",
+            "group": aio_id,
+        }
+        graph2 = lambda aio_id: {  # noqa: E731
+            "type": "Graph2",
+            "group": aio_id,
+        }
+        graph1_div = lambda aio_id: {  # noqa: E731
+            "type": "Graph1_div",
+            "group": aio_id,
+        }
+        graph2_div = lambda aio_id: {  # noqa: E731
+            "type": "Graph2_div",
             "group": aio_id,
         }
 
@@ -113,41 +108,100 @@ class SkeGraphAIO(html.Div):
         if aio_id is None:
             aio_id = str(uuid.uuid4())
 
-        if env.SERVER_URL:
-            title_div = html.Div(
-                [
-                    html.H2(title),
-                    dbc.NavLink(
-                        html.H3(
-                            html.I(className="bi bi-link"),
-                            title="Click a data point to get its URL,"
-                            + " then click here to open",
-                        ),
-                        id=self.ids.title_link(aio_id),
-                        target="_blank",
-                    ),
-                ],
-                style={
-                    "display": "flex",
-                    "margin": 0,
-                    "justify-content": "space-between",
-                },
-            )
-        else:
-            title_div = html.H2(title, style={"margin": 0})
+        title_div = html.Div(
+            children=[
+                html.H3(title),
+                dcc.Store(id=self.ids.title(aio_id), data=title),
+            ],
+            className="title-div",
+        )
 
-        super().__init__([title_div, dcc.Graph(id=self.ids.graph(aio_id), **kwargs)])
+        graph1_div = html.Div(
+            children=[
+                html.Div(
+                    html.I(
+                        className="bi bi-arrow-up-right-square-fill",
+                        title="Select a data point",
+                    ),
+                    id=self.ids.link1(aio_id),
+                    className="link-div",
+                ),
+                dcc.Graph(id=self.ids.graph1(aio_id), **kwargs),
+            ],
+            id=self.ids.graph1_div(aio_id),
+            className="graph-div",
+        )
+
+        graph2_div = html.Div(
+            html.P("Crossfilter disabled", className="lead"),
+            id=self.ids.graph2_div(aio_id),
+            className="graph-div",
+        )
+
+        super().__init__(
+            [
+                title_div,
+                graph1_div,
+                graph2_div,
+            ],
+            className="graph-group",
+        )
 
     @callback(
-        Output(ids.title_link(MATCH), "href"),
-        Input(ids.graph(MATCH), "clickData"),
+        Output(ids.link1(MATCH), "children"),
+        Input(ids.graph1(MATCH), "clickData"),
+        prevent_initial_call=True,
+    )
+    def update_link1(clickData):
+        if clickData:
+            _links1 = [_update_link(point) for point in clickData["points"]]
+            return sorted(_links1, key=lambda nav: nav.href)
+
+    @callback(
+        Output(ids.link2(MATCH), "children"),
+        Input(ids.graph2(MATCH), "clickData"),
+        prevent_initial_call=True,
     )
     def update_link(clickData):
-        if clickData and env.SERVER_URL:
-            ls = clickData.get("points")[0].get("customdata")
-            return cql_ttype_filter(
-                params=json.loads(ls[0]),
-                attr_path=ls[1],
-                value=ls[2],
-                base_url=env.SERVER_URL.rstrip("/") + "/#concordance?",
-            )
+        if clickData:
+            _links1 = [_update_link(point) for point in clickData["points"]]
+            return sorted(_links1, key=lambda nav: nav.href)
+
+    @callback(
+        Output(ids.graph2_div(MATCH), "children"),
+        Input(ids.graph1(MATCH), "clickData"),
+        Input("crossfilter-picker", "value"),
+        State("corpora-picker", "value"),
+        State("attribute-picker", "value"),
+        State("statistic-picker", "value"),
+        Input(ids.title(MATCH), "data"),
+    )
+    def make_graph2(clickdata, crossfilter, corpora, attribute, statistics, title):
+        id = ctx.outputs_list["id"] | {"type": "Graph2"}
+        if not crossfilter:
+            return html.P("Crossfilter disabled", className="lead")
+        if not clickdata:
+            return html.P("Select a data point to crossfilter", className="lead")
+
+        df, x_suffix = _df_from_crossfilter(clickdata, crossfilter, title)
+        if df.empty:
+            return html.P("Nothing found", className="lead")
+
+        def crossfilter_annotation(annotation):
+            if annotation.text.startswith("attr"):
+                annotation.update(text=f"{annotation.text} & {x_suffix}")
+
+        df = prep_data(corpora, crossfilter, [], statistics, df)
+        fig = bar_figure(df)
+        fig.for_each_annotation(crossfilter_annotation)
+        return [
+            html.Div(
+                html.I(
+                    className="bi bi-arrow-up-right-square-fill",
+                    title="No data point selected",
+                ),
+                id=id | {"type": "Link2"},
+                className="link-div",
+            ),
+            dcc.Graph(figure=fig, id=id | {"type": "Graph2"}),
+        ]
